9ed3f08cc9beda1d084eb1e265eeb675d98cb62c
kwrs99 <sch.kirsten11@gmail.com>
2023-04-29 13:09:54 +0200

    Fixed bug

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ function formatForecast(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = [
-    "Sundat",
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
